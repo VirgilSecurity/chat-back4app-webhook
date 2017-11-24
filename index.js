9939ddda2e3dc44678be766fcf5e5e7c38f1f5bc
Vadim Avdeev <v.avdeiev@gmail.com>
2017-11-24 18:08:29 +0200

    initial beforeSave webhook implementation

---
+++ index.js
@@ -0,0 +1,84 @@
+require('dotenv').configure();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const virgil = require('virgil-sdk');
+
+const webhookKey = process.env.WEBHOOK_KEY;
+
+function validateWebhookRequest(req, res, next) {
+  if (req.get('X-Parse-Webhook-Key') !== webhookKey) {
+    return errorResponse(res, 'Unauthorized Request.');
+  }
+
+  next();
+}
+
+function validateTrigger(triggerName, className) {
+  return function (req, res, next) {
+    if (triggerName !== req.body.triggerName
+      || className !== req.body.object.className) {
+      return errorResponse(res, 'Unknown trigger.');
+    }
+
+    next();
+  }
+}
+
+function successResponse(res, data) {
+  data = data || true;
+  res.status(200).send({ 'success' : data });
+}
+
+function errorResponse(res, message) {
+  message = message || true;
+  res.status(200).send({ 'error' : message });
+}
+
+const app = express();
+
+app.use(validateWebhookRequest);
+app.use(bodyParser.json());
+app.use(logger.dev());
+
+app.post('/users', validateTrigger('beforeSave', 'User'), function(req, res) {
+  const applicationKey = virgil.crypto.importPrivateKey(
+    process.env.APP_KEY,
+    process.env.APP_KEY_PASSWORD
+  );
+
+  const user = req.body.object;
+  const cardRequest = virgil.publishCardRequest.import(user.csr);
+  const signer = virgil.requestSigner(virgil.crypto);
+  signer.authoritySign(process.env.APP_ID, applicationKey);
+
+  const client = virgil.client(
+    process.env.VIRGIL_ACCESS_TOKEN,
+    {
+      cardsBaseUrl: process.env.VIRGIL_CARDS_URL
+    }
+  );
+
+  client.publishCard(cardRequest)
+    .then(card => {
+      user.virgilCardId = card.id;
+      delete user.csr;
+
+      successResponse(res, user);
+    })
+    .catch(e => {
+      console.error('Failed to publish Virgil Card', e);
+      errorResponse(res, 'Could not create Virgil Card');
+    });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.end('{ "error": "Request Failed."}');
+});
+
+const port = process.env.PORT || 443;
+app.listen(port, function() {
+  console.log('Webhooks server listening on port ' + port);
+});
